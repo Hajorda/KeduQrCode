6d8471f4867a087d09cf4f4b3cfe4d728b89886e
HajordaM <hajorda@gmail.com>
2024-09-24 17:47:28 +0300

    feat(main): Setup Screen add

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:scan/scan.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,154 +11,251 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
-  String aliNumber = "00004904";
+  String number = "";
   String qrData = "";
 
+  Future<void> getKey() async {
+    // Get Keys from Shared Preferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? key = prefs.getString("key");
+
+    if (key != null) {
+      setState(() {
+        number = key;
+        qrData = "$number,${DateFormat('yyyy-MM-dd').format(DateTime.now())} 23:59:00";
+      });
+    } else {
+      debugPrint("Key Not Found");
+      // show a dialog screen
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Key Not Found"),
+            content: const Text("You need to scan a qr code first!"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  parseQr(context);
+                },
+                child: const Text("Scan QR"),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
   void initState() {
-    //getKey();
     super.initState();
-    qrData =
-        "$aliNumber,${DateFormat('yyyy-MM-dd').format(DateTime.now())} 23:59:00";
+    getKey(); // Get the key when the widget is initialized
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("KEDU Qr Code"),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  //color: Color.fromARGB(255, 224, 227, 229),
-                ),
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundImage: AssetImage("lib/assets/images/image.png"),
-                      backgroundColor: Colors.transparent,
-                    ),
-                  ],
-                ),
+      appBar: AppBar(
+        title: const Text("KEDU Qr Code"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage("lib/assets/images/image.png"),
+                    backgroundColor: Colors.transparent,
+                  ),
+                ],
               ),
-              ListTile(
-                title: const Text("Scan Qr"),
-                // Scan Icon
-                leading: const Icon(Icons.qr_code_scanner),
-                onTap: () {
-                  // Show dialog for scan qr code
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text("Scan Qr Code"),
-                        content: const Text(
-                            "You need a screenshot of the qr, take a screenshot and upload it!"),
-                        actions: [
-                          TextButton(onPressed: () {
+            ),
+            ListTile(
+              title: const Text("Delete Key"),
+              leading: const Icon(Icons.delete),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Delete Key"),
+                      content: const Text("Are you sure you want to delete key?"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            SharedPreferences.getInstance().then((prefs) {
+                              prefs.remove("key");
+                              setState(() {
+                                number = ""; // Clear the number when key is deleted
+                                qrData = ""; // Clear the QR code
+                              });
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text("Key Deleted"),
+                                ),
+                              );
+                              Navigator.pop(context);
+                            });
+                          },
+                          child: const Text("Yes"),
+                        ),
+                        TextButton(
+                          onPressed: () {
                             Navigator.pop(context);
-                          }, child: const Text("Cancel")),
-                          TextButton(
+                          },
+                          child: const Text("No"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Scan Qr"),
+              leading: const Icon(Icons.qr_code_scanner),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Scan Qr Code"),
+                      content: const Text(
+                          "You need a screenshot of the qr, take a screenshot and upload it!"),
+                      actions: [
+                        TextButton(
                             onPressed: () {
                               Navigator.pop(context);
                             },
-                            child: GestureDetector(child: Text("Select Image"),
-                            onTap: () async{
-                              debugPrint("Select Image");
-                              // Open Image Picker
-                              final ImagePicker _picker = ImagePicker();
-                               final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-                                if (image != null) {
-                                  debugPrint("Image Selected");
-                                  // Read Image
-                                  // Read Qr Code
-                                  // Set Qr Code
-                                }
-                                else{
-                                  debugPrint("Image Not Selected");
-                                  // Snack Bar message
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                      content: Text("Image Not Selected"),
-                                    ),
-                                  );
-                                }
-                            },
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text("Info"),
-                // Info Icon
-                leading: const Icon(Icons.info),
-                onTap: () {
-                  // Show Dialog
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text("Info"),
-                        content: const Text(
-                            "This is a simple Qr Code Generator App for Kedu Edu. First Scan your qr code from your real application and after that you can use this app while entering gate."),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
+                            child: const Text("Cancel")),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: GestureDetector(
+                            child: const Text("Select Image"),
+                            onTap: () async {
+                              await parseQr(context);
                               Navigator.pop(context);
                             },
-                            child: const Text("OK"),
                           ),
-                        ],
-                      );
-                    },
-                  );
-                },
-              ),
-              // Switch for dark mode
-              SwitchListTile(
-                title: const Text("Dark Mode"),
-                value: false,
-                onChanged: (value) {},
-              ),
-              SwitchListTile(
-                title: const Text("Auto Set Wallpaper"),
-                value: false,
-                onChanged: (value) {},
-              ),
-            ],
-          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Info"),
+              leading: const Icon(Icons.info),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Info"),
+                      content: const Text(
+                          "This is a simple Qr Code Generator App for Kedu Edu. First Scan your qr code from your real application and after that you can use this app while entering gate."),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("OK"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+            SwitchListTile(
+              title: const Text("Dark Mode"),
+              value: false,
+              onChanged: (value) {},
+            ),
+            SwitchListTile(
+              title: const Text("Auto Set Wallpaper"),
+              value: false,
+              onChanged: (value) {},
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: QrImageView(
-
-                  version: QrVersions.auto,
-                  errorStateBuilder: (context, error) => const Text("Hata"),
-                  errorCorrectionLevel: QrErrorCorrectLevel.L,
-                  constrainErrorBounds: true,
-                  //embeddedImage: const AssetImage("lib/assets/images/image.png"),
-                  // embeddedImageStyle: const QrEmbeddedImageStyle(
-                  //   color: Colors.blue,
-                    // size: 500,
-                  // ),
-                  data: qrData,
-                ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: QrImageView(
+                version: QrVersions.auto,
+                errorStateBuilder: (context, error) => const Text("Error"),
+                errorCorrectionLevel: QrErrorCorrectLevel.L,
+                constrainErrorBounds: true,
+                data: qrData, // Display the QR code data
               ),
-              Text(qrData),
-            ],
+            ),
+            Text(qrData), // Display the QR code string
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> parseQr(BuildContext context) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      String? result = await Scan.parse(image.path);
+      if (result == null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Qr Code Not Found"),
           ),
-        ));
+        );
+      } else {
+        RegExp regExp = RegExp(r'^\d{8},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$');
+        if (regExp.hasMatch(result)) {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setString("key", result.split(",")[0]);
+          setState(() {
+            number = result.split(",")[0];
+            qrData = "$number,${DateFormat('yyyy-MM-dd').format(DateTime.now())} 23:59:00";
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Qr Code Saved"),
+            ),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Qr Code Not Valid"),
+            ),
+          );
+        }
+        Navigator.pop(context);
+      }
+      
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Image Not Selected"),
+        ),
+      );
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:scan/scan.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tedu_qrcode/HomePage.dart';
 import 'package:flutter/services.dart';
 
@@ -12,6 +15,92 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // WidgetsFlutterBinding.ensureInitialized();
+
+    Future<void> getKey() async{
+  // Get Keys from Shared Preferences
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? key = prefs.getString("key");
+  if(key == null){
+    debugPrint("Key Not Found");
+    // show a dialog screen
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Key Not Found"),
+          content: const Text("You need to scan a qr code first!"),
+          actions: [
+            TextButton(onPressed: () async{
+             debugPrint("Select Image");
+    // Open Image Picker
+    final ImagePicker _picker = ImagePicker();
+     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        debugPrint("Image Selected");
+        // Read Image
+        String? result = await Scan.parse(image.path);
+        debugPrint("Result: $result");
+        RegExp regExp = RegExp(r'^\d{8},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$');
+        if(result == null){
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Qr Code Not Found"),
+            ),
+          );
+        }
+        else{
+        if(regExp.hasMatch(result)){
+          // Save Qr Code
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          try{
+             await prefs.setString("key", result.split(",")[0]);
+             debugPrint("Key Saved: ${result.split(",")[0]}");
+          }
+          catch(e){
+            debugPrint("Error: $e");
+          }
+         
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Qr Code Saved"),
+            ),
+          );
+        }
+        else{
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Qr Code Not Valid"),
+            ),
+          );
+        }
+        }
+        Navigator.pop(context);
+       
+      }
+      else{
+        debugPrint("Image Not Selected");
+        // Snack Bar message
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Image Not Selected"),
+          ),
+        );
+      }
+            }, child: const Text("Scan QR")),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
    SystemChrome.setPreferredOrientations([
         DeviceOrientation.portraitUp,
         DeviceOrientation.portraitDown,
