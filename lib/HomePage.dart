af2d9745a30ef26da74852658670c814af6e7080
HajordaM <hajorda@gmail.com>
2024-09-27 17:09:52 +0300

    feat(dev): kinda working walpaper need to do help me pls

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:scan/scan.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,9 +17,70 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
   String number = "";
   String qrData = "";
+
+  bool isAutoWallpaperSet = false;
+
+  Future<File> setWalppaper() async {
+    if(qrData.isEmpty) {
+      debugPrint("Qr Data is empty");
+      return File("");
+    }
+    ByteData? qrImage = await QrPainter(
+      version: QrVersions.auto,
+      errorCorrectionLevel: QrErrorCorrectLevel.L,
+      data: qrData, // Display the QR code data
+      gapless: true,
+      color: Colors.white,
+    ).toImageData(878);
+
+    final buffer = qrImage?.buffer;
+    if(buffer == null) {
+      debugPrint("Buffer is null");
+      return File("");
+    }
+    if(qrImage == null) {
+      debugPrint("QrImage is null");
+      return File("");
+    }
+
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path;
+    var filePath = tempPath + '/file_01.tmp'; // file_01.tmp is dump file, can be anything
+     File assetPath = await File(filePath).writeAsBytes(
+        buffer.asUint8List(qrImage.offsetInBytes, qrImage.lengthInBytes));
+        return assetPath;
+
+   // const wallpaperLocation = WallpaperLocation.lockScreen;
+// With crop bounds
+    // const cropBounds = Rect.fromLTRB(100, 100, 200, 100);
+    // bool result = await WallpaperHandler.instance.setWallpaperFromAsset(
+    //     assetPath.path, wallpaperLocation,
+    //     // cropBounds: cropBounds
+    //     );
+
+//     int location = WallpaperManager.BOTH_SCREEN; //can be Home/Lock Screen
+// bool result = await WallpaperManager.setWallpaperFromFile(assetPath.path, location); //provide image path
+
+  }
+
+  
+  Future<void> wallpaperSetter() async{
+    File file = await setWalppaper();
+
+    int location = WallpaperManager.LOCK_SCREEN; //can be Home/Lock Screen
+   bool result = await WallpaperManager.setWallpaperFromFile(file.path, location); //provide image path
+
+
+    if (result) {
+      debugPrint("mutluyuz");
+    } else {
+      debugPrint("mutlu degiliz");
+    }
+  }
 
   Future<void> getKey() async {
     // Get Keys from Shared Preferences
@@ -23,7 +90,8 @@ class _HomePageState extends State<HomePage> {
     if (key != null) {
       setState(() {
         number = key;
-        qrData = "$number,${DateFormat('yyyy-MM-dd').format(DateTime.now())} 23:59:00";
+        qrData =
+            "$number,${DateFormat('yyyy-MM-dd').format(DateTime.now())} 23:59:00";
       });
     } else {
       debugPrint("Key Not Found");
@@ -59,11 +127,13 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     getKey(); // Get the key when the widget is initialized
+    WakelockPlus.enable();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         title: const Text("KEDU Qr Code"),
       ),
@@ -91,14 +161,16 @@ class _HomePageState extends State<HomePage> {
                   builder: (context) {
                     return AlertDialog(
                       title: const Text("Delete Key"),
-                      content: const Text("Are you sure you want to delete key?"),
+                      content:
+                          const Text("Are you sure you want to delete key?"),
                       actions: [
                         TextButton(
                           onPressed: () {
                             SharedPreferences.getInstance().then((prefs) {
                               prefs.remove("key");
                               setState(() {
-                                number = ""; // Clear the number when key is deleted
+                                number =
+                                    ""; // Clear the number when key is deleted
                                 qrData = ""; // Clear the QR code
                               });
                               ScaffoldMessenger.of(context).showSnackBar(
@@ -189,29 +261,42 @@ class _HomePageState extends State<HomePage> {
             ),
             SwitchListTile(
               title: const Text("Auto Set Wallpaper"),
-              value: false,
-              onChanged: (value) {},
+              value: isAutoWallpaperSet,
+              onChanged: (value) {
+                // Auto Set Wallpaper
+                setState(() {
+                  isAutoWallpaperSet = value;
+                 wallpaperSetter();
+                });
+              },
             ),
           ],
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: QrImageView(
-                version: QrVersions.auto,
-                errorStateBuilder: (context, error) => const Text("Error"),
-                errorCorrectionLevel: QrErrorCorrectLevel.L,
-                constrainErrorBounds: true,
-                data: qrData, // Display the QR code data
+      body: Builder(builder: (context) {
+        if (number.isEmpty) {
+          return const Center(
+            child: Text("No Key Found"),
+          );
+        }
+        return Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(60.0),
+                child: QrImageView(
+                  version: QrVersions.auto,
+                  errorStateBuilder: (context, error) => const Text("Error"),
+                  errorCorrectionLevel: QrErrorCorrectLevel.L,
+                  constrainErrorBounds: true,
+                  data: qrData, // Display the QR code data
+                ),
               ),
-            ),
-            Text(qrData), // Display the QR code string
-          ],
-        ),
-      ),
+              Text(qrData), // Display the QR code string
+            ],
+          ),
+        );
+      }),
     );
   }
 
@@ -233,7 +318,8 @@ class _HomePageState extends State<HomePage> {
           await prefs.setString("key", result.split(",")[0]);
           setState(() {
             number = result.split(",")[0];
-            qrData = "$number,${DateFormat('yyyy-MM-dd').format(DateTime.now())} 23:59:00";
+            qrData =
+                "$number,${DateFormat('yyyy-MM-dd').format(DateTime.now())} 23:59:00";
           });
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(
@@ -249,7 +335,6 @@ class _HomePageState extends State<HomePage> {
         }
         Navigator.pop(context);
       }
-      //?
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
@@ -259,3 +344,4 @@ class _HomePageState extends State<HomePage> {
     }
   }
 }
+// ????
--- lib/main.dart
+++ lib/main.dart
@@ -15,104 +15,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // WidgetsFlutterBinding.ensureInitialized();
-
-    Future<void> getKey() async{
-  // Get Keys from Shared Preferences
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? key = prefs.getString("key");
-  if(key == null){
-    debugPrint("Key Not Found");
-    // show a dialog screen ?
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Key Not Found"),
-          content: const Text("You need to scan a qr code first!"),
-          actions: [
-            TextButton(onPressed: () async{
-             debugPrint("Select Image");
-    // Open Image Picker
-    final ImagePicker _picker = ImagePicker();
-     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-      if (image != null) {
-        debugPrint("Image Selected");
-        // Read Image
-        String? result = await Scan.parse(image.path);
-        debugPrint("Result: $result");
-        RegExp regExp = RegExp(r'^\d{8},\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$');
-        if(result == null){
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text("Qr Code Not Found"),
-            ),
-          );
-        }
-        else{
-        if(regExp.hasMatch(result)){
-          // Save Qr Code
-          SharedPreferences prefs = await SharedPreferences.getInstance();
-          try{
-             await prefs.setString("key", result.split(",")[0]);
-             debugPrint("Key Saved: ${result.split(",")[0]}");
-          }
-          catch(e){
-            debugPrint("Error: $e");
-          }
-         
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text("Qr Code Saved"),
-            ),
-          );
-        }
-        else{
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text("Qr Code Not Valid"),
-            ),
-          );
-        }
-        }
-        Navigator.pop(context);
-       
-      }
-      else{
-        debugPrint("Image Not Selected");
-        // Snack Bar message
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Text("Image Not Selected"),
-          ),
-        );
-      }
-            }, child: const Text("Scan QR")),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text("Cancel"),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-   SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255)),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          brightness: Brightness.light,
+          primary: const Color.fromARGB(255, 0, 0, 0),
+          secondary: const Color.fromARGB(255, 0, 0, 0),
+          surface: const Color.fromARGB(255, 255, 255, 255),
+        ),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+            brightness: Brightness.dark,
+            primary: Colors.grey[900],
+            secondary: Colors.grey[800],
+            surface: const Color.fromARGB(255, 255, 252, 252)),
         useMaterial3: true,
       ),
       home: const HomePage(),
     );
   }
-} //?
-//!
+}
